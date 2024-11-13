b140d72c25c9e884fb3a36da6a7ce54d325db240
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-13 22:53:43 +0100

    format line

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
   int n = int.parse(args[0]);
-  assert(n>=80, 'The score must be bigger or equal to 80');
+  assert(n >= 80, 'The score must be bigger or equal to 80');
   print('You passed');
 }
