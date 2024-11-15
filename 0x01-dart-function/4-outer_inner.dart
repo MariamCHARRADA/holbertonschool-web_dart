ab1253a5fb9eaee9a7c1b980ce372d939444a3c3
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-15 18:47:58 +0100

    string interpolation fix

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,7 @@
 void outer(String name, String id) {
   String inner() {
     String lastName = name.split(" ")[1];
-    return "Hello Agent $lastName[0].$name your id is $id";
+    return "Hello Agent ${lastName[0]}.$name your id is $id";
   }
   print(inner());
 }
