d5d69d1d3c5ca2937f230bb799848fb87059da3d
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-15 18:52:24 +0100

    firstName fix

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,8 @@
 void outer(String name, String id) {
   String inner() {
     String lastName = name.split(" ")[1];
-    return "Hello Agent ${lastName[0]}.${name} your id is $id";
+    String firstName = name.split(" ")[0];
+    return "Hello Agent ${lastName[0]}.${firstName} your id is $id";
   }
   print(inner());
 }
