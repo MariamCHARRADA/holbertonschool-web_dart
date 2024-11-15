9c7f0ce0106fd17e1c06717b99a93209eadf7534
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-15 18:45:47 +0100

    var type

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,6 @@
 void outer(String name, String id) {
   String inner() {
-    lastName = name.split(" ")[1];
+    String lastName = name.split(" ")[1];
     return "Hello Agent $lastName[0].$name your id is $id";
   }
   print(inner());
