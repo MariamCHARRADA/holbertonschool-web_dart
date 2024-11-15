f68f4588aeb5ea5993affa8f30632cde542eaf1a
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-15 23:11:05 +0100

    !

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,6 +1,6 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int pointsA = teamA['Free throws'] * 1 + teamA['2 pointers'] * 2 + teamA['3 pointers'] * 3;
-  int pointsB = teamB['Free throws'] * 1 + teamB['2 pointers'] * 2 + teamB['3 pointers'] * 3;
+  int pointsA = teamA['Free throws']! + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3 ;
+  int pointsB = teamB['Free throws']! * 1 + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
   
   if (pointsA > pointsB) {
     return 1;
