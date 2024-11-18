43328066761bb0573022ad6a1b8003aa3458691e
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-18 22:00:36 +0100

    sanchez

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacters() async {
+  const String apiUrl = 'https://rickandmortyapi.com/api/character';
+  try {
+    var response = await http.get(Uri.parse(apiUrl));
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+
+    List characters = jsonData['results'];
+    for (var character in characters) {
+      print(character['name']);
+    }
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
