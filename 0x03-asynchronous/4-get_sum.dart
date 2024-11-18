1b09d28bd94c64b86512d5b0105a9ff304cd5467
MariamCHARRADA <charradamariam@yahoo.com>
2024-11-18 21:40:43 +0100

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+    Map user = jsonDecode(userData);
+    String userId = user['id'];
+
+    String userOrders = await fetchUserOrders(userId);
+    List orders = jsonDecode(userOrders);
+
+    double total = 0;
+    for (var product in orders) {
+      String productPrice = await fetchProductPrice(product);
+      total += jsonDecode(productPrice);
+    }
+
+    return total;
+  } catch (error) {
+    return -1;
+  }
+ 
+}
